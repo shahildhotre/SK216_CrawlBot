c14afd770a041473c271371fc157a81f5eecf436
mihirpanchal17 <panchalmihir17@gmail.com>
2020-08-02 19:18:34 +0530

    Contains wordcloud and has feature to flag a page

---
+++ Dashboard/flag.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Crawl Bot</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <?php
+    require_once("slidebar.php")
+    ?>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <!-- Main Content -->
+       
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+
+
+      <div class="container" style="padding-top: 10px">
+        <h1 style="display: inline-block; text-align: center;padding-left: 35%">Crawl Bot</h1>
+      </div>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
+    			
+              <!-- Page Heading -->
+              <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                <h1 class="h3 mb-0 text-gray-800">Flag A Page</h1>
+              </div>
+
+              <div class="row">
+
+            <!-- Content Column -->
+            <div class="col-xl-8 col-lg-5">
+              <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <span class="m-0 font-weight-bold text-primary">Word-Cloud
+                    <div class="dropdown">
+                      <button class="dropbtn" style="width: 500px; margin-left: 20px">http://stationciqmfldhh.onion</button>
+                      <div class="dropdown-content font-weight-bold text-primary" style="width: 500px; margin-left: 20px">
+                        <a href="flag.php?val=flag">http://stationciqmfldhh.onion</a>
+                        <a href="flag1.php?val=flag">http://deepmartyqzffl5n.onion</a>
+                        <a href="flag2.php?val=flag">http://jf4wcdcbcrjtatk42nj7a2haruihvxjcu3xcyl5yipj5whphjklmplad.onion</a>
+                      </div>
+                    </div>
+                  </span>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                  <div class="cloud pt-4 pb-2" style="margin-top: 0px; text-align: center;">
+                    <img src="img/wordcloud/cloud1.PNG" height="430" width="500" align="center">
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4 mb-4">
+
+              <!-- Illustrations -->
+              <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <span class="m-0 font-weight-bold text-primary">Flag A Page</span>
+                </div>
+
+                <!-- Card Body -->
+                <div class="card-body">
+                  <div class="cloud pt-4 pb-2 font-weight-bold text-primary">
+                    <span>
+                      <label><input type="radio" name = 'status'>  To watch</label>
+                      <br>
+                      <label><input type="radio" name = 'status'>  To report</label>
+                      <br>
+                      <label><input type="radio" name = 'status'>  To take action</label>
+                      
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+
+
+              
+        </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+  <!-- Page level plugins -->
+  <script src="vendor/chart.js/Chart.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  
+  <script src="js/demo/chart-pie-demo.js"></script>
+
+</body>
+
+</html>
+
+
+
