4f69ddb90514e91e294d143c7c6796ff0721d01f
mihirpanchal17 <panchalmihir17@gmail.com>
2020-08-02 19:21:02 +0530

    Link details for link map

---
+++ Dashboard/linkref.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Crawl Bot</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <?php
+    require_once("slidebar.php")
+    ?>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <!-- Main Content -->
+       
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+
+
+      <div class="container" style="padding-top: 10px">
+        <h1 style="display: inline-block; text-align: center;padding-left: 35%">Crawl Bot</h1>
+      </div>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
+        
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Link Details</h1>
+          </div>
+
+          <div class="row">
+            <div class="col-xl-12 col-lg-7">
+                <div class="card shadow mb-4">
+                  <!-- Card Header - Dropdown -->
+                  <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <span class="m-0 font-weight-bold text-primary">
+                      Back link: <br>
+                      B1: http://jf4wcdcbcrjtatk42nj7a2haruihvxjcu3xcyl5yipj5whphjklmplad.onion <br>
+                      B2:   http://so52qlpuhdjxiz2agd7gbzuklaj5ivgzfhgcke7ufw3rluamnyz2mjqd.onion <br>
+                      B3:   http://necro.ecmgchy4u3tx53slfobz4jh43nsgtubsr4qoz65late5wjw42hvl2aqd.onion <br>
+                      B4:   http://darkdirrkuz4jghve4yp7e2mnhspsfjgxoc7hmc3rlykhoixnetqemad.onion <br>
+                      B5: http://ma4e4zm4fxgm2wdimseqaqha4ovx2bjat6f5c64gduisi2lb44ccy3qd.onion <br>
+                          <br>
+                      Main link: <br>
+
+                      Main:   http://loliporns5lm2t3lvojm6k7jjhduhpsty43g7izoerx475jtuticpiqd.onion <br>
+                          <br>
+                      Child links: <br>
+                                                                                             
+                      C1: http://stationudue7ucifsaxjf3ln6pnv5klsm7ntoju4k3253eluzcebu7qd.onion/ <br>
+                      C2: http://cpmovtv5lco5622p.onion <br>
+                      C3:   http://ehids454tbdl7qeip3f2chw4iw2hsm6aqyeof6cz2glhrenqrhh53tyd.onion/ehindex.php <br>
+                      C4:   http://bonanza2yxypu6zkxlhrqbqbde5tivw4eqxuefshpe24yh7jhmaa4iad.onion <br>
+                      C5:   http://pkbrrpm2uq4hz52pt7u52ocwemake5fscfztsjlhg6fu5tkfadkupjyd.onion <br>
+                      C6:   http://nkmxbnup44toysy5na7zqoq2bomuwt34akapfp62lbscdptaiim7void.onion <br>
+                      C7:   http://ilpcnenvpxok3q47id7i3swqy7iuqmtzhxrttvh3q6jpqc6rbjpc7rqd.onion <br>
+                      C8:   http://6ftkr3u7zhywzy5jaly2vscpsmetfu7jmrevqmevftygr75pr6nfv6qd.onion  <br>
+                          <br>
+                      Child to Child links: <br>
+
+                      C21:  http://cpmovtv5lco5622p.onion/login.php <br>
+                      C22:  http://cpmovtv5lco5622p.onion/buy.php <br>
+                      C23:  http://cpmovtv5lco5622p.onion/index.php <br>
+                      C24:  http://cpmovtv5lco5622p.onion/pedo/page1.php <br>
+                    </span>
+                  </div>
+                </div>
+            </div>
+          </div>
+
+       </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+  <!-- Page level plugins -->
+  <script src="vendor/chart.js/Chart.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  
+  <script src="js/demo/chart-pie-demo.js"></script>
+
+</body>
+
+</html>
