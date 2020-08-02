babbb6c57223e01a7375f801d7feb5f510731114
mihirpanchal17 <panchalmihir17@gmail.com>
2020-08-02 18:54:06 +0530

    Checking database to see newly added links

--- Crawlbot/active_links.php
+++ Crawlbot/active_links.php
@@ -1,15 +1,37 @@
-Search Results
-<br>
 <?php
-if(file_exists("crawledlinks.csv")){
-	$CSVfp = fopen("crawledlinks.csv", "r");
-	if($CSVfp !== FALSE) {
-	 while(! feof($CSVfp)) {
-	  $data = fgetcsv($CSVfp, 1000, ",");
-	  $linkdisp = "<a href=".$data[1]." style='color:white;'>".$data[1]."</a>";
-	  echo $linkdisp;
-	  echo "<br>";
-	 }
+
+$dbServername = "localhost:3308";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "sih";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+$sql = "SELECT * FROM dark WHERE `link_status` ='Active'";
+$result = mysqli_query($conn,$sql);
+$num = mysqli_num_rows($result);
+
+$sql_surface = "SELECT * FROM surface WHERE `link_status` ='Active'";
+$result_surface = mysqli_query($conn,$sql_surface);
+$num_surface = mysqli_num_rows($result_surface);
+
+echo "Search Results";
+echo "<br>";
+
+if($num>0){
+	while($row = $result->fetch_assoc()){
+		$linkdisp = "<a href=".$row['link_url']." style='color:white; margin-left:20px; font-size:1.2em;'>".$row['link_url']."</a>";
+		echo $linkdisp;
+		echo "<br>";
+		echo "<br>";
 	}
-	fclose($CSVfp);
+}
+
+if($num_surface>0){
+	while($row = $result_surface->fetch_assoc()){
+		$linkdisp = "<a href=".$row['link_url']." style='color:white; margin-left:20px; font-size:1.2em;'>".$row['link_url']."</a>";
+		echo $linkdisp;
+		echo "<br>";
+		echo "<br>";
+	}
 }
